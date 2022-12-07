2e96641233f523cc075491bd98c184b3d53eb149
chandan.singh@scaler.com <chandan.singh@scaler.com>
2022-12-07 10:44:46 +0530

    some changes

--- cohorts/scoreboard.py
+++ cohorts/scoreboard.py
@@ -3,14 +3,44 @@ from commons.redis import performRedisOps
 from commons.cost_tracker import track_db_hit
 
 def fetchRankListFromDB(cohort_id):
+    """
+    Please don't update this method. Use this method to get scoreboard from DB.
+    Parameters:
+        cohort_id (int): CohortId
+  
+    Returns:
+    list: list of tuple with (email, score) sorted by score desc
+        ex: [('user1@example.com', 100), ('user2@example.com', 10)]
+    """
+
     track_db_hit()
     return CohortUser.objects.filter(
         score__gt=0,
         cohort_id=cohort_id
     ).order_by('-score').values_list('user__email', 'score')
 
+
 def FetchRankList(cohort_id):
+    """
+    Fetch Rank list of Cohort
+    Parameters:
+        cohort_id (int): CohortId
+  
+    Returns:
+    list: list of tuple with (email, score) sorted by score desc
+        ex: [('user1@example.com', 100), ('user2@example.com', 10)]
+    """
+    # Complete this function, using given helper functions 'performRedisOps' and 'fetchRankListFromDB'
     pass
 
+
 def updateUserRank(user_email, cohort_id, new_score):
+    """
+    Update Rank list in Cache, shouldn't make call to DB.
+    Parameters:
+        user_email (str): Email for User whose score is updated in DB.
+        cohort_id (int): CohortId
+        cohort_id (int): Assigned Score
+    """
+    # Complete this function
     pass
--- commons/redis.py
+++ commons/redis.py
@@ -6,5 +6,12 @@ redis_client = redis.StrictRedis('localhost', 6379, charset="utf-8", decode_resp
 
 # Please don't update these methods. These methods will be replaced at runtime.
 def performRedisOps(operation, *args):
+    """
+    Please don't update this method. Use this method to get scoreboard from DB.
+    Use Ex: performRedisOps("zrevrange", "some_random_key", "0", "-1")
+    Parameters:
+        operation (str): Redis command. ex: set, get, zadd, expire
+        args: All other arguments, Required for Redis command.
+    """
     track_redis_hit()
     return getattr(redis_client, operation)(*args)
